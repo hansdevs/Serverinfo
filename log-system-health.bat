b02e5731e039aa79f67bc8840ef95606b22c1b8d
Hans Gamlien <120060905+hansdevs@users.noreply.github.com>
2025-03-07 00:08:24 -0700

    Update log-system-health.bat

--- log-system-health.bat
+++ log-system-health.bat
@@ -1,4 +1,8 @@
 echo @echo off > testscript.bat
-echo echo Testing file creation >^> created.txt >> testscript.bat
-echo echo File created. >> testscript.bat
+echo cd /d E:\Serverinfo >> testscript.bat
+echo echo Hello from Git test > testfile.txt >> testscript.bat
+echo git add -A >> testscript.bat
+echo git commit -m "Test commit" >> testscript.bat
+echo git push origin main >> testscript.bat
+echo echo Done! >> testscript.bat
 echo pause >> testscript.bat
